b3c01e3e2a3a553c64173e0056a7a705bbc464f5
n0500 <t720711@qsmg.moe.gov.sa>
2026-08-28 23:50:24 +0300

    Add PDF export functionality using jsPDF and html2canvas

---
+++ src/lib/pdfExport.js
@@ -0,0 +1,29 @@
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
+export async function exportElementToPdf(element, filename) {
+  const canvas = await html2canvas(element, { scale: 2, useCORS: true, backgroundColor: '#ffffff' });
+  const imgData = canvas.toDataURL('image/png');
+
+  const pdf = new jsPDF({ orientation: 'p', unit: 'mm', format: 'a4' });
+  const pageWidth = pdf.internal.pageSize.getWidth();
+  const pageHeight = pdf.internal.pageSize.getHeight();
+
+  const imgWidth = pageWidth;
+  const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+  let heightLeft = imgHeight;
+  let position = 0;
+
+  pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+  heightLeft -= pageHeight;
+
+  while (heightLeft > 0) {
+    position = heightLeft - imgHeight;
+    pdf.addPage();
+    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+  }
+
+  pdf.save(filename);
+}
